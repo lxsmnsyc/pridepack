286aac8a142f197ace8517a0d802896640368506
Alexis Munsayac <alexis.munsayac@gmail.com>
2020-09-11 17:55:49 +0800

    Add jest config to .pridepackrc

--- src/utils/default-config.ts
+++ src/utils/default-config.ts
@@ -20,7 +20,7 @@
  * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
  * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
  * SOFTWARE.
- */
+ */import { InitialOptions } from '@jest/types/build/Config';
 
 export interface PridepackBaseConfig {
   srcDir: string;
@@ -34,6 +34,7 @@ export interface PridepackConfig extends PridepackBaseConfig {
   target: string;
   jsxFactory?: string;
   jsxFragment?: string;
+  jest?: InitialOptions;
 }
 
 const DEFAULT_CONFIG: PridepackConfig = {
