fc9b5a78684bbb1201d3b3d0e5b375c07382c489
Alexis Munsayac <alexis.munsayac@gmail.com>
2020-10-01 02:22:37 +0800

    Add root program

---
+++ src/program/Program.tsx
@@ -0,0 +1,66 @@
+import { render } from 'ink';
+import React from 'react';
+import Build from './Build';
+import Clean from './Clean';
+import CreatePackage from './Init/CreatePackage'
+import InitPackage from './Init/InitPackage';
+
+export type ProgramCommand =
+  | 'build'
+  | 'create'
+  | 'init'
+  | 'test'
+  | 'watch'
+  | 'clean'
+  | 'check'
+  | 'lint'
+
+export interface ProgramProps {
+  command: ProgramCommand;
+  template?: string;
+  packageName?: string;
+}
+
+function Program(
+  { command, template, packageName }: ProgramProps,
+) {
+  if (command === 'create' && packageName) {
+    return (
+      <CreatePackage
+        template={template ?? 'basic'}
+        packageName={packageName}
+      />
+    );
+  }
+  if (command === 'init') {
+    return (
+      <InitPackage
+        template={template ?? 'basic'}
+      />
+    );
+  }
+  if (command === 'clean') {
+    return <Clean />;
+  }
+  if (command === 'build') {
+    return <Build />;
+  }
+
+  return <></>
+}
+
+export default function renderProgram(
+  command: ProgramCommand,
+  template?: string,
+  packageName?: string,
+) {
+  render((
+    <Program
+      command={command}
+      template={template}
+      packageName={packageName}
+    />
+  ), {
+    patchConsole: true,
+  });
+}
