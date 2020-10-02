2dc09d06ae55a158d1aebec3527bdaea962c103e
Alexis Munsayac <alexis.munsayac@gmail.com>
2020-10-02 18:41:19 +0800

    Remove old files

--- src/program/index.ts
+++
@@ -1,9 +0,0 @@
-import renderProgram, { ProgramCommand } from './Program';
-
-export default function runProgram(
-  command: ProgramCommand,
-  template?: string,
-  packageName?: string,
-): void {
-  renderProgram(command, template, packageName);
-}
