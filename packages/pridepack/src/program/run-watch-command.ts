fbf7cbf50c2dd5506648811d76aed851120f2aba
Alexis Munsayac <alexis.munsayac@gmail.com>
2021-10-01 21:39:04 +0800

    Rework `watch`

--- packages/pridepack/src/program/run-watch-command.ts
+++ packages/pridepack/src/program/run-watch-command.ts
@@ -1,42 +1,43 @@
 import runBuildCJS from './run-build-cjs';
 import runBuildESM from './run-build-esm';
 import generateTSDiagnostics from './generate-ts-diagnostics';
-import generateESBuildDiagnostics from './generate-esbuild-diagnostics';
 import watchCompileTypes from '../core/watch-compile-types';
+import runTask from './run-task';
 
-export default async function runWatchCommand(): Promise<() => void> {
-  const cjs = await runBuildCJS(true);
-  const esm = await runBuildESM(true);
+export default async function runWatchCommand(
+  onRebuild?: () => void,
+): Promise<void> {
+  let stopTS: () => void;
+  const task = await runTask(async () => {
+    const cjs = await runBuildCJS(true);
+    const esm = await runBuildESM(true);
 
-  const stopTS = watchCompileTypes(
-    (diagnostic) => {
-      generateTSDiagnostics([diagnostic]);
+    async function rebuild() {
+      await cjs.dev.start();
+      await cjs.prod.start();
+      await esm.dev.start();
+      await esm.prod.start();
+    }
+
+    stopTS = watchCompileTypes(
+      (diagnostic) => {
+        generateTSDiagnostics([diagnostic]);
+      },
+      () => {
+        rebuild().catch(console.error);
+        onRebuild?.();
+      },
+      false,
+    );
+  }, {
+    pending: 'Watch mode starting...',
+    success: 'Watching files...',
+    failure: 'Watch mode failed.',
+  }, {
+    onStop() {
+      stopTS();
     },
-    () => {
-      Promise.all([
-        cjs.dev.rebuild(),
-        cjs.prod.rebuild(),
-        esm.dev.rebuild(),
-        esm.prod.rebuild(),
-      ]).then(([cjsDev, cjsProd, esmDev, esmProd]) => {
-        generateESBuildDiagnostics(false, cjsDev.warnings);
-        generateESBuildDiagnostics(true, cjsDev.warnings);
-        generateESBuildDiagnostics(false, cjsProd.warnings);
-        generateESBuildDiagnostics(true, cjsProd.warnings);
-        generateESBuildDiagnostics(false, esmDev.warnings);
-        generateESBuildDiagnostics(true, esmDev.warnings);
-        generateESBuildDiagnostics(false, esmProd.warnings);
-        generateESBuildDiagnostics(true, esmProd.warnings);
-      }, console.error);
-    },
-    false,
-  );
+  });
 
-  return () => {
-    stopTS();
-    cjs.dev.stop();
-    cjs.prod.stop();
-    esm.dev.stop();
-    esm.prod.stop();
-  };
+  await task.start();
 }
