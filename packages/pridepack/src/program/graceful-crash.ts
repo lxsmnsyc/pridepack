02200ab10e294b1c8664026cac75473d04ad64fe
Alexis Munsayac <alexis.munsayac@gmail.com>
2021-10-18 21:28:11 +0800

    Remove win32 shim

--- packages/pridepack/src/program/graceful-crash.ts
+++ packages/pridepack/src/program/graceful-crash.ts
@@ -1,16 +1,3 @@
-import { createInterface } from 'readline';
-
-if (process.platform === 'win32') {
-  const rl = createInterface({
-    input: process.stdin,
-    output: process.stdout,
-  });
-
-  rl.on('SIGINT', () => {
-    process.emit('SIGINT', 'SIGINT');
-  });
-}
-
 process.on('SIGINT', () => {
   process.exit(1);
 });
