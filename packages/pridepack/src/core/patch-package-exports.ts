9504d5d26a8444c89b8d16ecc5405f42ad217255
Alexis Munsayac <alexis.munsayac@gmail.com>
2022-06-30 11:44:10 +0800

    Add support for `typesVersions`

--- packages/pridepack/src/core/patch-package-exports.ts
+++ packages/pridepack/src/core/patch-package-exports.ts
@@ -1,3 +1,4 @@
+import path from 'path';
 import { PridepackConfig } from './default-config';
 import { outputJson } from './fs-utils';
 import getPackagePath from './get-package-path';
@@ -26,9 +27,11 @@ export default async function patchPackageExports(
   const packageInfo = await readPackage(cwd);
   const jsx = config.jsx === 'preserve' ? 'x' : '';
   const entries: Record<string, ExportEntry> = {};
+  const targetTSPaths: Record<string, [string]> = {};
   // eslint-disable-next-line no-restricted-syntax
   for (const moduleEntry of Object.keys(config.entrypoints)) {
     const tsPath = await getTypesTarget(config.entrypoints[moduleEntry]);
+    const typesPath = `./${toPosix(tsPath)}.d.ts`;
     entries[moduleEntry] = {
       development: {
         require: `./${toPosix(getCJSTargetDirectory(moduleEntry, true))}${jsx}`,
@@ -36,12 +39,20 @@ export default async function patchPackageExports(
       },
       require: `./${toPosix(getCJSTargetDirectory(moduleEntry, false))}${jsx}`,
       import: `./${toPosix(getESMTargetDirectory(moduleEntry, false))}${jsx}`,
-      types: `./${toPosix(tsPath)}.d.ts`,
+      types: typesPath,
     };
+    const targetPath = moduleEntry === '.' ? '*' : path.relative('.', moduleEntry);
+    targetTSPaths[targetPath] = [typesPath];
   }
 
   await outputJson(
     getPackagePath(cwd),
-    { ...packageInfo, exports: entries },
+    {
+      ...packageInfo,
+      exports: entries,
+      typesVersions: {
+        '*': targetTSPaths,
+      },
+    },
   );
 }
