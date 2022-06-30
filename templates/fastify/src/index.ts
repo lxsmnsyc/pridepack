30de3c57109835f23c80263db876912b03ac433e
Alexis Munsayac <alexis.munsayac@gmail.com>
2022-06-30 17:17:20 +0800

    Update fastify template

--- templates/fastify/src/index.ts
+++ templates/fastify/src/index.ts
@@ -3,7 +3,7 @@ import SERVER from './server';
 
 import './routes';
 
-SERVER.listen(8080, '0.0.0.0', (err, address) => {
+SERVER.listen({ port: 8080, host: '0.0.0.0' }, (err, address) => {
   if (err) {
     SERVER.log.error(err.message);
     process.exit(1);
