19a0bf75f15529e5c19aecfc1d6f2ff765c4e9ce
Alexis Munsayac <alexis.munsayac@gmail.com>
2023-03-25 13:04:34 +0800

    Update patch-package-exports.ts

--- packages/pridepack/src/core/patch-package-exports.ts
+++ packages/pridepack/src/core/patch-package-exports.ts
@@ -1,3 +1,4 @@
+import path from 'path';
 import { PridepackConfig } from './default-config';
 import { outputJson } from './fs-utils';
 import getExtensionJS from './get-extension-js';
@@ -77,7 +78,8 @@ export default async function patchPackageExports(
       mainTarget = entries[moduleEntry].require;
       moduleTarget = entries[moduleEntry].import;
     } else {
-      targetTSPaths[moduleEntry] = [typesPath];
+      const resolved = path.posix.relative('.', moduleEntry);
+      targetTSPaths[resolved] = [typesPath];
     }
   }
 
