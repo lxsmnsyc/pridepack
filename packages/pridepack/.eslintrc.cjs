ec26ced4386a29c3e6a5f200dc0ac8885e347830
Alexis Munsayac <alexis.munsayac@gmail.com>
2023-08-23 11:33:52 +0800

    Fix eslint config

--- packages/pridepack/.eslintrc.cjs
+++ packages/pridepack/.eslintrc.cjs
@@ -8,10 +8,7 @@ module.exports = {
     "tsconfigRootDir": __dirname,
   },
   "rules": {
-    "no-param-reassign": "off",
     "@typescript-eslint/no-unsafe-assignment": "off",
     "@typescript-eslint/no-unsafe-argument": "off",
-    "no-await-in-loop": "off",
-    "no-restricted-syntax": "off"
   },
 };
